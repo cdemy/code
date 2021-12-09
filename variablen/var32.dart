8c2b64d44ff4d4e8c3a315b27b424ebf6e5a5da6
cdemy Leitung <88116396+cdemyLeitung@users.noreply.github.com>
2021-12-09 09:23:51 +0100

    variablen03 complete

--- variablen/var32.dart
+++ variablen/var32.dart
@@ -1,3 +1,25 @@
 void main() {
-  // final datum = DateTime();
+  const wochentage = {
+    1: 'Montag',
+    2: 'Dienstag',
+    3: 'Mittwoch',
+    4: 'Donnerstag',
+    5: 'Freitag',
+    6: 'Samstag',
+    7: 'Sonntag'
+  };
+  final today = DateTime.now();
+  print('Heute ist der ${today.day}.${today.month}.${today.year}');
+  print('Es ist ${today.hour}:${today.minute} Uhr.');
+  print('Heute ist ein ${wochentage[today.weekday]}.');
+
+  final tomorrow = today.add(Duration(days: 1));
+  print('Morgen ist der ${tomorrow.day}.${tomorrow.month}.${tomorrow.year}.');
+
+  final firstOfMonth = DateTime(today.year, today.month, 1);
+  print('Der Erste dieses Monats war ein ${wochentage[firstOfMonth.weekday]}.');
+
+  final firstOfNextMonth = DateTime(today.year, today.month + 1, 1);
+  final lastOfMonth = firstOfNextMonth.subtract(Duration(days: 1));
+  print('Dieser Monat hat übrigens ${lastOfMonth.day} Tage.');
 }
