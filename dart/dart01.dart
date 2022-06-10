710bd0f2e684bb466d57d2823627537d6a519b67
AUSoftAndreas <andreas_hoefer@cdemy.de>
2022-06-10 17:55:10 +0200

    x

---
+++ dart/dart01.dart
@@ -0,0 +1,10 @@
+void main1() {
+  int a = 1;
+  String b = 'Hallo';
+  double c = 7 / 3;
+  bool d = true;
+  print('${a.runtimeType}: $a');
+  print('${b.runtimeType}: $b');
+  print('${c.runtimeType}: $c');
+  print('${d.runtimeType}: $d');
+}
---
+++ dart/dart02.dart
@@ -0,0 +1,10 @@
+void main() {
+  var a = 1;
+  var b = 'Hallo';
+  var c = 7 / 3;
+  var d = true;
+  print('${a.runtimeType}: $a');
+  print('${b.runtimeType}: $b');
+  print('${c.runtimeType}: $c');
+  print('${d.runtimeType}: $d');
+}
