17df03f8d7552ff3501b64d332215b06b0b973c4
AUSoftAndreas <andreas_hoefer@cdemy.de>
2023-10-16 09:16:18 +0200

    x

--- variablen/var05.dart
+++ variablen/var05.dart
@@ -1,24 +1,24 @@
-// // ignore_for_file: unused_local_variable
+// ignore_for_file: unused_local_variable
 
-// void main() {
-//   var stringDurchZuweisung = "Hallo";
-//   var laengeVonStringDurchZuweisung = stringDurchZuweisung.length;
+void main() {
+  var stringDurchZuweisung = "Hallo";
+  var laengeVonStringDurchZuweisung = stringDurchZuweisung.length;
 
-//   String stringDurchTypbezeichnung;
-//   stringDurchTypbezeichnung = "Hallo";
-//   var laengeVonStringDurchTypbezeichnung = stringDurchTypbezeichnung.length;
+  String stringDurchTypbezeichnung;
+  stringDurchTypbezeichnung = "Hallo";
+  var laengeVonStringDurchTypbezeichnung = stringDurchTypbezeichnung.length;
 
-//   var dynamicString;
-//   dynamicString = "Hallo";
-//   var laengeVonDynamicString = dynamicString.length;
+  var dynamicString;
+  dynamicString = "Hallo";
+  var laengeVonDynamicString = dynamicString.length;
 
-//   var intDurchZuweisung = 5;
-//   var laengeVonIntDurchZuweisung = intDurchZuweisung.length; // Die IDE hilft
+  // var intDurchZuweisung = 5;
+  // var laengeVonIntDurchZuweisung = intDurchZuweisung.length; // Die IDE hilft
 
-//   var intInDynamic;
-//   intInDynamic = 5;
-//   var laengeVonIntInDynamic = intInDynamic.length; // Die IDE hilft nicht!!!
+  var intInDynamic;
+  intInDynamic = 5;
+  var laengeVonIntInDynamic = intInDynamic.length; // Die IDE hilft nicht!!!
 
-//   // Fehler 1 fällt in der IDE auf, also uns beim Schreiben des Codes
-//   // Fehler 2 fällt erst auf, wenn die Zeile ausgeführt wird, also zB beim Kunden!!!
-// }
+  // Fehler 1 fällt in der IDE auf, also uns beim Schreiben des Codes
+  // Fehler 2 fällt erst auf, wenn die Zeile ausgeführt wird, also zB beim Kunden!!!
+}
